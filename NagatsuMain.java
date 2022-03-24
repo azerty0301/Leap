c64b197bae30cea93c99e17dda93f2fdf2a2de4a
okaka <aki.na@live.jp>
2022-03-24 12:21:30 +0900

    うるう年判定追加

---
+++ NagatsuMain.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class NagatsuMain{
+	public static void main(String[] args){
+		System.out.print("西暦>>");
+		int year = new Scanner(System.in).nextInt();
+		if(year % 400 == 0){
+		System.out.println("年はうるう年です");
+		}else if(year % 100 == 0){
+		System.out.println("年はうるう年ではありません");
+		}else if(year % 4 == 0){
+		System.out.println("年はうるう年です");
+		}else{
+		System.out.println("年はうるう年ではありません");
+		}
+	}
+}
