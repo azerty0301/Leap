cc25d7d5a86b95f1a6122d5066e08bd192552655
cshm <cshm225@gmail.com>
2022-03-24 11:48:10 +0900

    Matsuoka作成

---
+++ Matsuoka.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Matsuoka{
+	public static void main(String[] args){
+		int year;
+		System.out.print("西暦>>");
+		year=new Scanner(System.in).nextInt();
+		if(year%400==0 || year%100!=0 && year%4==0){
+		System.out.println("西暦>>"+year+"は閏年です");
+		}else{
+		System.out.println("西暦>>"+year+"は閏年ではありません");
+		}
+	}
+}
